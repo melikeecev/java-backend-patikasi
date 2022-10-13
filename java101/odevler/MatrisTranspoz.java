337bc54839f90c48ee338da413ef20f7e55825cc
Melike Çevik <80070068+melikeecev@users.noreply.github.com>
2022-10-13 20:53:51 +0300

    odev

---
+++ java101/odevler/MatrisTranspoz.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class MatrisTranspoz {
+
+    public static void main(String[] args) {
+        int[][] arr = {{2, 3, 4}, {5, 6, 4}}; 
+
+        System.out.println("Matris: ");
+        for (int i = 0; i < arr.length; i++) { 
+            for (int j = 0; j < arr[0].length; j++) { 
+                System.out.print(arr[i][j] + "  ");
+            }
+            System.out.println("");
+        }
+
+        System.out.println("Transpoze:");
+        for (int i = 0; i < arr[0].length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                System.out.print(arr[j][i] + "  ");
+            }
+            System.out.println("");
+        }
+    }
+}
